f334932a6986e02b67d774ecde3bed548a33ab8e
Fritz Meyer <meyerfr@gmx.de>
2019-12-23 18:20:44 +0100

    added phone codes and changed few stylings

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,7 @@ class PaymentsController < ApplicationController
   end
 
   def new
-    @phone_code = ['+49', '+34', '+43']
+    @phone_code = %w(+61 +43 +32 +55 +1 +86 +45 +358 +33 +49 +852 +353 +39 +81 +352 +52 +31 +64 +47 +351 +65 +34 +46 +41 +44)
     @booking = Booking.find(params[:booking_id])
   end
 
