1e56c3de9d1ecd8412fae74ac51316435c8729f6
Fritz Meyer <meyerfr@gmx.de>
2019-12-30 11:54:09 +0100

    created one file for list style and created parter#index page

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,6 +1,12 @@
 class PartnersController < ApplicationController
   skip_before_action :authenticate_user!
+
+  def index
+    @partners = Partner.all
+  end
+
   def new
+    @phone_code = %w(+61 +43 +32 +55 +1 +86 +45 +358 +33 +49 +852 +353 +39 +81 +352 +52 +31 +64 +47 +351 +65 +34 +46 +41 +44)
     @partner = Partner.new
   end
 
