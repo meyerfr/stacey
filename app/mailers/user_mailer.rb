0ffb23d9fb7fcf80ca5f6d79b2673e87e7e143e0
Fritz Meyer <meyerfr@gmx.de>
2020-01-06 14:58:40 +0100

    added stacey logo to all mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,13 +4,16 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.welcome_call.subject
   #
+  before_action :set_logo_attachment
+
   def welcome(booking)
     @booking = booking
     @user = @booking.user
     if WelcomeCall.find_by(booking_id: @booking.id)
       message.perform_deliveries = false
     else
-      mail(to: @user.email, subject: 'Stacey - coliving')
+      email_with_name = %("#{@user.full_name}" <#{@user.email}>)
+      mail(to: email_with_name, subject: 'Stacey - coliving')
     end
   end
 
@@ -18,13 +21,21 @@ class UserMailer < ApplicationMailer
     @welcome_call = welcome_call
     @booking = @welcome_call.booking
     @user = @booking.user
-    mail(to: @user.email, subject: 'Stacey - coliving - welcome call')
+    email_with_name = %("#{@user.full_name}" <#{@user.email}>)
+    mail(to: email_with_name, subject: 'Stacey - coliving - Call invitation')
   end
 
   def welcome_call_rescheduled(welcome_call)
     @welcome_call = welcome_call
     @booking = @welcome_call.booking
     @user = @booking.user
-    mail(to: @user.email, subject: 'Stacey - coliving - welcome call')
+    email_with_name = %("#{@user.full_name}" <#{@user.email}>)
+    mail(to: email_with_name, subject: 'Stacey - coliving - welcome call')
+  end
+
+  private
+
+  def set_logo_attachment
+    attachments.inline['logo.png'] = File.read("#{Rails.root}/app/assets/images/stacey_logo_pink.png")
   end
 end
